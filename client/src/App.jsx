0b7ec5d8a1543c84fec44977293d19e20dffaad2
sankalp <badonisankalp123@gmail.com>
2025-03-09 14:56:05 +0530

    fixed navabr issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './routes/Home'
 import Dashboard from './routes/Dashboard'
 import Generator from './routes/Generator'
 
+const Layout = ({ children }) => {
+  const location = useLocation()
+  const isGeneratorPage = location.pathname === '/generator'
+
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
+      {!isGeneratorPage && <Navbar />}
+      <main>
+        {children}
+      </main>
+      {!isGeneratorPage && <Footer />}
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/generator" element={<Generator />} />
-           
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/generator" element={<Generator />} />
+        </Routes>
+      </Layout>
     </Router>
   )
 }
