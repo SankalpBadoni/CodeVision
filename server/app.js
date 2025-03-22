b504fe4b849f9ab1a281c947ff673fd804ce4a58
sankalp <badonisankalp123@gmail.com>
2025-03-23 01:29:09 +0530

    integrated MongoDB with Mongoose for user management; added user model for registration and authentication; updated app.js to include authentication routes and MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import generatorRoutes from './routes/generator.js';
-
+import mongoose from 'mongoose';
+import authRoutes from './routes/auth.route.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.log(err);
+})
 
 // Routes
 app.use('/api', generatorRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
