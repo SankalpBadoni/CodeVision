efa6e0a53fe7b397b17d34929ef628bd81643999
sankalp <badonisankalp123@gmail.com>
2025-04-12 00:20:46 +0530

    cors prod fix 3

--- server/app.js
+++ server/app.js
@@ -13,20 +13,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://code-vision-two.vercel.app",
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    'http://localhost:5173',
+    'https://code-vision-two.vercel.app',
+  ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  // exposedHeaders: ['set-cookie'],
 }));
 app.use(express.json());
 app.use(cookieParser()); 
